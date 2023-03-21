9e6f640c1330efd9a4815574355788dcebd83469
Ray Ploski <ray.ploski@gmail.com>
2023-03-21 13:12:29 -0400

    Updates to formatting and removal of tutorial prefixes.

--- hcp-vault-vpc/vault.tf
+++ hcp-vault-vpc/vault.tf
@@ -1,12 +1,14 @@
-resource "hcp_hvn" "learn_hcp_vault_hvn" {
+# HVNs enable you to deploy HashiCorp Cloud products without the need 
+# to manage networking details
+resource "hcp_hvn" "hcp_vault_hvn" {
   hvn_id         = var.hvn_id
   cloud_provider = var.cloud_provider
   region         = var.region
 }
 
-resource "hcp_vault_cluster" "learn_hcp_vault" {
-  hvn_id     = hcp_hvn.learn_hcp_vault_hvn.hvn_id
-  cluster_id = var.cluster_id
-  tier       = var.tier
-  # public_endpoint = true
+resource "hcp_vault_cluster" "hcp_vault" {
+  hvn_id          = hcp_hvn.hcp_vault_hvn.hvn_id
+  cluster_id      = var.cluster_id
+  tier            = var.tier
+  public_endpoint = true
 }
--- hcp-vault-vpc/vpc-peering.tf
+++ hcp-vault-vpc/vpc-peering.tf
@@ -11,15 +11,15 @@ data "aws_arn" "peer" {
 }
 
 resource "hcp_aws_network_peering" "peer" {
-  hvn_id              = hcp_hvn.learn_hcp_vault_hvn.hvn_id
-  peering_id          = var.peering_id
-  peer_vpc_id         = aws_vpc.peer.id
-  peer_account_id     = aws_vpc.peer.owner_id
-  peer_vpc_region     = data.aws_arn.peer.region
+  hvn_id          = hcp_hvn.hcp_vault_hvn.hvn_id
+  peering_id      = var.peering_id
+  peer_vpc_id     = aws_vpc.peer.id
+  peer_account_id = aws_vpc.peer.owner_id
+  peer_vpc_region = data.aws_arn.peer.region
 }
 
 resource "hcp_hvn_route" "peer_route" {
-  hvn_link         = hcp_hvn.learn_hcp_vault_hvn.self_link
+  hvn_link         = hcp_hvn.hcp_vault_hvn.self_link
   hvn_route_id     = var.route_id
   destination_cidr = aws_vpc.peer.cidr_block
   target_link      = hcp_aws_network_peering.peer.self_link
